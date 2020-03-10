b598fdb0e77fe3b8470cbd027128fc4000f0595f
Goh Keat Liang <keatliang2005@gmail.com>
2020-03-10 14:20:29 +0800

    proper migration name fix

---
+++ src/migrations/2016_05_19_000000_create_journals_table.php
@@ -0,0 +1,37 @@
+<?php
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class CreateUsersTable
+ */
+class CreateJournalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('journals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('ledger_id')->nullable();
+            $table->bigInteger('balance');
+            $table->string('currency',5);
+	        $table->string('morphed_type',32);
+	        $table->integer('morphed_id');
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('journals');
+    }
+}
